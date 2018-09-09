04ff097492c7740bdb9b38f3b1e67c3f09eebb53
Tomohiko Himura <eiel.hal@gmail.com>
2018-09-09 17:43:59 +0900

    chore: added ExActor

--- mix.exs
+++ mix.exs
@@ -22,8 +22,9 @@ defmodule Osh.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:chatwork_ex, "~> 0.0.1"},
+      {:exactor, "~> 2.2.4", warn_missing: false},
       {:timex, "~> 3.1"},
-      {:chatwork_ex, "~> 0.0.1"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
