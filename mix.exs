c8cac9b1926cc5be5dcbb6895cca8b21738b9729
Tomohiko Himura <eiel.hal@gmail.com>
2018-09-09 14:12:31 +0900

    chore: add chatwork_ex

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule Osh.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:timex, "~> 3.1"}
+      {:timex, "~> 3.1"},
+      {:chatwork_ex, "~> 0.0.1"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
