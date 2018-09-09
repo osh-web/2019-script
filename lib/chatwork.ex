a4994986ae734b999e14905846f8f8719212c950
Tomohiko Himura <eiel.hal@gmail.com>
2018-09-09 23:02:50 +0900

    チャットワーク投稿時のログを追加

--- lib/chatwork.ex
+++ lib/chatwork.ex
@@ -5,10 +5,13 @@ defmodule OSH.Chatwork do
 
   use ExActor.GenServer, export: {:global, __MODULE__}
 
+  require Logger
+
   defstart start_link(state), do: initial_state(state)
 
   defcast post(message, room_id), state: access_token do
     ChatworkEx.Endpoint.Rooms.Messages.post!(access_token, room_id, message)
+    Logger.info("post message to ChatWork #{inspect({room_id, message})}")
     new_state(access_token)
   end
 end
